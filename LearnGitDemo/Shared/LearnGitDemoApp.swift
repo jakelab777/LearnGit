539578335acaab7c2894ec2ec475b20acf1228e3
jake <thedoctor17th@outlook.com>
2021-02-09 19:21:10 +0800

    First Commit

---
+++ LearnGitDemo/Shared/LearnGitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitDemoApp.swift
+//  Shared
+//
+//  Created by Jake James on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
